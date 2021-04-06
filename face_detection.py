702b41444b0cfdd8494c4c87f81cd329931117ee
jcisarano <cisarano@gmail.com>
2021-04-06 08:08:26 -0400

    scale image and convert to grayscale using cv2

--- face_detection.py
+++ face_detection.py
@@ -2,3 +2,13 @@ import cv2
 
 image = cv2.imread("Images/people1.jpg")
 print(image.shape)
+
+#scale image`
+image = cv2.resize(image,(int(image.shape[1]*0.5),int(image.shape[0]*0.5)))
+print(image.shape)
+
+#convert to grayscale
+image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("",image_gray)
+cv2.waitKey(0)
