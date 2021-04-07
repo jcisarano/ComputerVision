479a56054877a4f39d035ffc206a4001035cf8d4
jcisarano <cisarano@gmail.com>
2021-04-07 08:15:12 -0400

    experiment with scale factor

--- face_detection.py
+++ face_detection.py
@@ -3,8 +3,9 @@ import cv2
 image = cv2.imread("Images/people1.jpg")
 print(image.shape)
 
-#scale image`
-image = cv2.resize(image,(int(image.shape[1]*0.5),int(image.shape[0]*0.5)))
+#scale image
+#image = cv2.resize(image,(int(image.shape[1]*0.5),int(image.shape[0]*0.5)))
+image = cv2.resize(image,(800,600))
 print(image.shape)
 
 #convert to grayscale
@@ -14,8 +15,10 @@ image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 #cv2.waitKey(0)
 
 face_detector = cv2.CascadeClassifier("Cascades/haarcascade_frontalface_default.xml")
-detections = face_detector.detectMultiScale(image_gray)
+
 #detections are positions of faces (x,y,width,height)
+#scale factor can be used to allow for different image size and size of expected faces
+detections = face_detector.detectMultiScale(image_gray, scaleFactor=1.09)
 print(detections)
 print(len(detections))
 
