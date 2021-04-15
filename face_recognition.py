f6a583e47b6f31cb0f7bcb63f2be8639c9003ad1
jcisarano <cisarano@gmail.com>
2021-04-15 08:11:17 -0400

    added dataset and started file

---
+++ face_recognition.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import cv2
+import numpy as np
+
+#import zipfile
+#path = "Datasets/yalefaces.zip"
+#zip_object = zipfile.ZipFile(file=path,mode='r')
+#zip_object.extractall("./")
+#zip_object.close()
