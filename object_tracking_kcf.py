6506fe231a8e7b4d5a2e02feadede10ca8fc7bef
jcisarano <cisarano@gmail.com>
2021-05-14 08:18:04 -0400

    completed video object tracking KCF

--- object_tracking_kcf.py
+++ object_tracking_kcf.py
@@ -11,3 +11,17 @@ print(ok)
 
 while True:
 	ok, frame = video.read()
+	if not ok:
+		break
+	ok, bbox = tracker.update(frame)
+	#print(bbox)
+	
+	if ok:
+		(x, y, w, h) = [int(v) for v in bbox]
+		cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0), 2,1)
+	else:
+		cv2.putText(frame, 'Error', (100,80),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+		
+	cv2.imshow("Tracking", frame)
+	if cv2.waitKey(1) & 0XFF == 27: # ESC key
+		break 
